0b6d6516b3ea9914d94282084e858d42476fef44
LIza denisenok <121236070+Not-the-essence@users.noreply.github.com>
2023-12-21 21:42:23 +0300

    Update 05-regex-tasks.js

--- src/05-regex-tasks.js
+++ src/05-regex-tasks.js
@@ -1,13 +1,9 @@
-
 function getRegexForGuid() {
   return /{\b[0-9A-F]{8}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{12}\b}/i;
 }
-
-
 function getRegexForPitSpot() {
   return /^.*p.t\w*/;
 }
-
 
 function getPasswordValidator(minLength) {
   const regex = new RegExp(`^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d]{${minLength},}$`);
